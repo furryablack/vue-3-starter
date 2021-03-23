e44f6253136ef9f1a0bced9a3b9eae1c9d0c0db3
Furrya Black <furrya.black@gmail.com>
2021-03-23 21:53:01 +0300

    change entry content
    there service's external link to smart protest

--- app-source/entry.vue
+++ app-source/entry.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>"Носите уже камни в карманах" @free_russia @free_navalny</div>
+    <div><a href="https://free.navalny.com/" title="free navalny" target="_blank">free navalny/</a></div>
 </template>
 
 <style src="@/ui/platform/prelude/all" lang="less"/>
